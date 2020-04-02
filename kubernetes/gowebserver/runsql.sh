5301b558ea0487bdf99261ffcd60faa6a84a4edf
Rich Anderson <richanderson00@gmail.com>
2020-04-01 20:09:35 -0400

    license...use this however you want

--- kubernetes/gowebserver/runsql.sh
+++ kubernetes/gowebserver/runsql.sh
@@ -2,7 +2,7 @@ CONTAINERID=$(docker ps | grep k8s_mysql | rev | cut -d ' ' -f 1 | rev)
 echo "mysql container id: " $CONTAINERID
 
 # copy the latest db.sql to the container
-docker cp db.sql $CONTAINERID:/db.sql
+docker cp ../../db/db.sql $CONTAINERID:/db.sql
 
 # run the sql scriptcopy the latest db.sql to the container
 
