948c3e61ab9f379b899f22faff2b03109297b25d
Rich Anderson <richanderson00@gmail.com>
2020-05-30 10:39:27 -0400

    fixed exception error.  added smooth fade across fade.  added mobile styles

---
+++ public/index.js
@@ -0,0 +1,73 @@
+
+var app = new Vue({
+    el: '#app',
+    data: {
+      todos: [ "Enter a message and then hit Submit. ", "It will be sent to the server, stored on the messages list and then sent back with any previous messages" ],
+      message: ""
+    }
+  })
+
+function loaded() {
+
+    var xhr = createCORSRequest('GET', '/greeting');
+
+    xhr.onload = function(body) {
+        var resp  = JSON.parse(xhr.responseText)
+        app.message = resp.Greeting;
+     };
+
+    xhr.onerror = function() {
+        alert('Woops, there was an error call the greeting api.');
+    };
+
+    xhr.send();
+}
+
+function echo() {
+    var url = '/echo';
+    var xhr = createCORSRequest('POST', url);
+
+    xhr.onload = function() {
+        var resp  = JSON.parse(xhr.responseText)
+        if (resp.StatusCode == 0 ) {
+            // hide the form so you can't post again, but say thank you.
+            app.todos = resp.PreviousMessages;
+        } else {
+            alert("ok dear, we hit a snag....StatusCode:" + resp.StatusCode + " error: " + resp.ErrorMessage);
+        }
+     };
+
+    xhr.onerror = function() {
+        alert('Woops, there was an error making the request.');
+    };
+
+    var jsonRequest = { 
+        "Message": document.forms['myForm'].message.value,
+    }
+
+    xhr.send(JSON.stringify(jsonRequest))
+
+    // clear the input
+    document.forms['myForm'].message.value = "";
+  
+    return false;
+}
+
+function createCORSRequest(method, url) {
+    var xhr = new XMLHttpRequest();
+    
+    if ("withCredentials" in xhr) {
+        // XHR for Chrome/Firefox/Opera/Safari.
+        xhr.open(method, url, true);
+    } 
+    else if (typeof XDomainRequest != "undefined") {
+        // XDomainRequest for IE.
+        xhr = new XDomainRequest();
+        xhr.open(method, url);
+    } 
+    else {
+        // CORS not supported.
+        xhr = null;
+    }
+    return xhr;
+}
