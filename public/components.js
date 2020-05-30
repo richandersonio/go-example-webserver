1d29200f9212e581ee186ba6ff41737232f60a8e
Rich Anderson <richanderson00@gmail.com>
2020-05-30 15:07:40 -0400

    added a basic header and footer component.  header does basic page level navigation

--- public/components.js
+++ public/components.js
@@ -19,6 +19,47 @@ Vue.component('my-header', {
     template: '<h3>I am a header</h3>'
 });
 
+
+Vue.component('my-header', {
+    props: ['active'],
+    data: function () {
+      return {
+        active: 0
+      }
+    },
+    template: `
+    <header class="header">
+        <div class="header-content">
+            <div class="header-logo">
+                <a href="/">
+                    <img src="images/logo.jpeg">
+                </a>
+            </div>
+            <nav class="header-nav" role="navigation">
+                <div>
+                    <ul class="header-nav">
+                        <li>
+                            <a href="/index.html" v-bind:class="active == 0 ? 'header_a_selected' : 'header_a' ">Welcome</a>
+                        </li>
+                        <li>
+                            <a href="/agenda.html"  v-bind:class="active == 1 ? 'header_a_selected' : 'header_a' ">Agenda</a>
+                        </li>
+                        <li>
+                            <a href="/news.html"  v-bind:class="active == 2 ? 'header_a_selected' : 'header_a' ">News</a>
+                        </li>
+                    </ul>
+                </div>
+            </nav>
+            <div class="header-updates">
+                <a class="header_a" href="/#subscribe" data-ga-click="Top nav, click, cta_text: open Subscribe modal">
+                    <span>Subscribe for Updates</span>
+                </a>
+		    </div>
+        </div>
+    </header>
+          `
+});
+
 Vue.component('my-footer', {
     data: function () {
       return {
