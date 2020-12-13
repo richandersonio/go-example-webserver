c0e74d994c1670d946b7c765ad692d6f7d2c2bc2
Rich Anderson <richanderson00@gmail.com>
2020-12-13 14:00:47 -0500

    systemctl is-active will return error code 3 if a service is not active.  Do an echo after it so the code deploy does not error out

--- admin/stop_service.sh
+++ admin/stop_service.sh
@@ -1,3 +1,4 @@
 # https://www.linode.com/docs/quick-answers/linux/start-service-at-boot/
 echo "stopping our service..."
 sudo systemctl is-active --quiet mygowebserverservice && systemctl stop mygowebserverservice
+echo "stopped"
