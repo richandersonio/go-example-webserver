09df9fd8c3b6299efcdee1638114179cac1a4ab9
Rich Anderson <richanderson00@gmail.com>
2020-12-13 13:48:21 -0500

    check if service is running before stopping it

--- admin/stop_service.sh
+++ admin/stop_service.sh
@@ -1,3 +1,3 @@
 # https://www.linode.com/docs/quick-answers/linux/start-service-at-boot/
 echo "stopping our wonderful service..."
-sudo systemctl stop mygowebserverservice
+sudo systemctl is-active --quiet mygowebserverservice && systemctl stop mygowebserverservice
